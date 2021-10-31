69335383b0d83896a4decf44502508297f8ed4ed
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2021-10-31 21:33:49 +0900

    Create 1550.c

---
+++ 1550.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() { 
+	int i;
+	scanf("%x", &i);	
+	printf("%d", i);
+}
+// %x = 16진수 입력값 (HEX)
+// %o = 8진수 입력값 (OCT)
+// %d = 10진수 입력값 (DEC)
