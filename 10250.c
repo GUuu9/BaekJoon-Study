34d82bdf07aed951329b9050c1ff64b5fb09fadd
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2021-11-04 23:28:13 +0900

    Create 10250.c

---
+++ 10250.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int testNum, h, w, guest, i, roomf = 0, roomn = 0;
+	scanf("%d", &testNum);
+	for (i = 0; i < testNum; i++) {
+		scanf("%d %d %d", &h, &w, &guest);	
+		if (h == 1) {
+			roomf = h;
+			roomn = guest;
+		}
+		else if (h * w == guest) {
+			roomf = h;
+			roomn = w;
+		}
+		else {
+			roomn = guest / h + 1;
+			roomf = guest % h;
+		}
+		if (roomn < 10)
+			printf("%d0%d\n", roomf, roomn);
+		else
+			printf("%d%d\n", roomf, roomn);
+	}
+}
+/* 
+호텔에 투숙하는 인원은 1호실인 층이 모두 차야 2호실을 이용한다.
+1층인 경우 층수는 무조건 높이 값 h = 1 이고 오는 순서대로 호실을 채운다.
+마지막 손님의 경우 최고 층수 최고 호실을 배정 받는다.
+나머지의 경우 손님 번호를 높이로 나누어 1을 더한 값으로 방번호를 구해주고(나머지가 없는 경우가 1호실이기 때문에 1을 더한다),
+나머지 값이 층수가 된다.
+호실의 표현은 HHWW이므로 10보다 작은 값인 경우 앞에 0을 붙혀서 출력해준다.
+*/
