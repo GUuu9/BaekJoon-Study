6994e0880048460911d59ff7a7e032227b91c98d
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2021-11-20 15:50:33 +0900

    Update 1929.c

--- 1929.c
+++ 1929.c
@@ -1,3 +1,10 @@
+
+
+
+
+
+// 1차시도
+/*
 #include<stdio.h>
 int main() {
 	int m, n, sum = 0, i, j;
@@ -16,3 +23,27 @@ int main() {
 	    }
 	}
 }
+*/
+
+// 2차시도
+/*
+#include<stdio.h>
+int main() {
+	int m, n, sum = 0, i, j, check;
+	scanf("%d\n%d", &m, &n);
+	//짝수 값의 경우 2가 아닌 나머지는 모두 소수가 아니므로 제외한다.
+	if(m != 2 && m%2 == 0)
+	    m++;
+	if(n != 2 && n%2 == 0)
+	    n--;
+	// 2씩 더해서 for문의 연산 횟수를 최대한 줄여준다.
+	for(i=m;i<=n;i+=2){
+	    for(j=3;j<=i;j+=2){
+	        if(i==j)
+	           printf("%d\n", i);
+	        if(i%j == 0)
+	           break;
+	    }
+	}
+}
+*/
