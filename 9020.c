aa89852b2ffee7072daffa605f3aeefd1e8bf8eb
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2021-11-22 14:33:15 +0900

    Create 9020.c

---
+++ 9020.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main() {
+    int T, n, i, j, k, l, a, b, x, y;
+    scanf("%d", &T);
+    for(i=0; i<T;i++){
+        scanf("%d", &n);
+        j=n/2;
+        a=j;
+        b=j;
+        while(1){
+            for(k=2;k*k<=a;k++){
+                if(a%k==0)
+                    break;
+            }
+            for(l=2;l*l<=b;l++){
+                if(b%l==0)
+                    break;
+            }
+            if(a%k!=0&&b%l!=0)
+                break;
+                
+            a--;b++;
+        }
+        printf("%d %d\n", a, b);
+    }
+}
