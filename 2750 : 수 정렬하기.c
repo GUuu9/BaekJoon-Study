895387f42e3e22b8114af49dda4ccf2934072b04
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2021-11-27 23:02:12 +0900

    Create 2750 : 수 정렬하기.c

---
+++ 2750 : 수 정렬하기.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int comp(const void* a, const void* b);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    qsort(arr, n, sizeof(int),comp);
+    
+    for(int i=0;i<n;i++){
+        printf("%d\n", arr[i]);
+    }
+}
+
+int comp(const void* a, const void* b){
+    return(*(int*)a - *(int*)b);
+}
