da252dae05170193ac7e42e345daec8896f299fa
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2022-01-27 22:41:08 +0900

    Create 2167.py

---
+++ 2167.py
@@ -0,0 +1,18 @@
+n, m = map(int,input().split())
+a = [list(map(int,input().split())) for _ in range(n)]
+
+k = int(input())
+for __ in range(k):
+    sum = 0
+    i, j, x, y = map(int,input().split())
+    z = j
+    while i<x or j<y:
+        sum += a[i-1][j-1]
+        j+=1
+        if j>y:
+            j=z
+            i+=1
+    
+    sum += a[x-1][y-1]
+    
+    print(sum)
