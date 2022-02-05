9ef14e9497fe7b53aa28a5b4c9455c32478feb7c
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2022-02-05 09:12:14 +0900

    Update 11728.py

--- 11728.py
+++ 11728.py
@@ -8,6 +8,24 @@ b = list(map(int,input().split()))
 
 a += b
 
+a= sorted(a)
+
+for _ in range(n+m):
+    print(a[_], end = ' ')
+
+'''
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+a += b
+
 a = sorted(a)
 
 print(a[:])
+'''
