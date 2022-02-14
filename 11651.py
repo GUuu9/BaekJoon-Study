aff38014da8879d25b2b22f0e49e3fb9ea0607a2
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2022-02-14 15:17:26 +0900

    Create 11651.py

---
+++ 11651.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+list_n = [[0]*2 for _ in range(N)]
+for _ in range(N):
+    list_n[_][1], list_n[_][0] = map(int,input().split())
+list_n = sorted(list_n)
+for _ in range(N):
+    print(list_n[_][1], list_n[_][0])
